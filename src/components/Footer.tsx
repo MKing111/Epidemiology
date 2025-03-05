b21d66928a06fea949b8210bbfb10ad1d1e3d272
MKing111 <109007817+MKing111@users.noreply.github.com>
2025-03-04 22:36:10 -0500

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+  return (
+    <div id="footer">Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import Icon from "../assets/dna.svg"
+const Sidebar = () => {
+    return (
+        <div id="Sidebar">
+            <img className='logo' src={Icon} alt="Logo" />
+            Epidemiology App
+        </div>
+    )
+}
+
+export default Sidebar
